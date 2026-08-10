3c174e3629f1c1cd2b8a6740dabd5a830e03ef93
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-09 21:08:35 -0700

    chore: enforce ultracite

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,23 +1,23 @@
-import {expectType, expectError} from 'tsd';
-import hasPermission, {assertPermission, PermissionError} from './index.js';
+import { expectError, expectType } from "tsd";
+import hasPermission, { assertPermission, PermissionError } from "./index.js";
 
 // HasPermission
-expectType<boolean>(hasPermission('fs.read'));
-expectType<boolean>(hasPermission('fs.read', '/tmp'));
-expectType<boolean>(hasPermission('fs.write', '/var/log'));
+expectType<boolean>(hasPermission("fs.read"));
+expectType<boolean>(hasPermission("fs.read", "/tmp"));
+expectType<boolean>(hasPermission("fs.write", "/var/log"));
 
 // AssertPermission
-expectType<void>(assertPermission('fs.read'));
-expectType<void>(assertPermission('fs.read', '/tmp'));
+expectType<void>(assertPermission("fs.read"));
+expectType<void>(assertPermission("fs.read", "/tmp"));
 
 // PermissionError
-const error = new PermissionError('fs.read', '/etc/passwd');
+const error = new PermissionError("fs.read", "/etc/passwd");
 expectType<PermissionError>(error);
 expectType<string>(error.scope);
 expectType<string | undefined>(error.reference);
 expectType<string>(error.message);
 
-const errorNoReference = new PermissionError('child');
+const errorNoReference = new PermissionError("child");
 expectType<string | undefined>(errorNoReference.reference);
 
 // Invalid usage
